0faba0874c086ffd2687bb067ac18ae92683b519
pranush-reddy <pranush.reddy0102@gmail.com>
2024-05-16 21:56:36 +0530

    piggymind main ui

---
+++ ta.js
@@ -0,0 +1,192 @@
+let t=0;
+let c=[];
+function val(event) {
+    event.preventDefault();
+    let a = document.getElementById("cat").value;
+    let b = document.getElementById("sp").value;
+    let errorMessage = document.getElementsByTagName("h5")[0];
+    if (a.length != 0 && b.length != 0) {
+        t = t + Number(b);
+        errorMessage.innerHTML = "";
+        const node = document.createElement("tr");
+        c++;
+        node.setAttribute("id", c);
+        const chnode0 = document.createElement("td");
+        const chb = document.createElement("input");
+        chb.setAttribute("id", c);
+        chb.setAttribute("class", "cb");
+        chb.setAttribute("type", "checkbox");
+        chnode0.appendChild(chb);
+        node.appendChild(chnode0);
+        chb.addEventListener("click", Uncheck);
+        const chnode1 = document.createElement("td");
+        let l = document.getElementsByTagName("tr").length;
+        let len = document.createTextNode(l);
+        chnode1.appendChild(len);
+        node.appendChild(chnode1);
+        const chnode2 = document.createElement("td");
+        let textA = document.createTextNode(a);
+        chnode2.setAttribute("id", "categ");
+        chnode1.setAttribute("id", "sn");
+        let imgSrc = "";
+        if (a === "Food") {
+            imgSrc = "food.svg";
+        } else if (a === "Transport") {
+            imgSrc = "transport.svg";
+        } else if (a === "Party") {
+            imgSrc = "party.svg";
+        } else if (a === "Groceries") {
+            imgSrc = "groceries.svg";
+        } else if (a === "Clothing") {
+            imgSrc = "clothing.svg";
+        } else if (a === "Bills") {
+            imgSrc = "bills.svg";
+        } else if (a === "Health care") {
+            imgSrc = "healthcare.svg";
+        } else if (a === "Entertainment") {
+            imgSrc = "entertainment.svg";
+        }
+        if (imgSrc !== "") {
+            let categoryImg = document.createElement("img");
+            categoryImg.setAttribute("src", imgSrc);
+            categoryImg.setAttribute("width", "15px");
+            chnode2.appendChild(categoryImg);
+            chnode2.innerHTML += "&nbsp;&nbsp;&nbsp;";
+        }
+        chnode2.appendChild(textA);
+        node.appendChild(chnode2);
+        const chnode3 = document.createElement("td");
+        chnode3.setAttribute("id", "spent");
+        chnode3.setAttribute("class", b);
+        let textB = document.createTextNode(b + " /-");
+        chnode3.appendChild(textB);
+        node.appendChild(chnode3);
+        document.getElementById("mytable").appendChild(node);
+        Update();
+        Remve();
+    }
+}
+
+
+
+function Uncheck(){
+    var op=document.querySelectorAll('input[type="checkbox"]');
+    for(var y=1;y<op.length;y++){
+        if(op[y].checked==false){
+           let mn= document.getElementById('mc');            //if anycheckbox is uncheckd main cb is uncheck
+            mn.checked=false;
+            break;
+        }
+    }
+}
+function Remve(){
+    let dv=document.querySelectorAll('input[type="checkbox"]');
+    for(var b=0;b<=dv.length;b++){                                  //to remove checkbox.ched after adding elementor deleting
+        dv[b].checked=false;
+    }
+}
+
+function del(){
+    
+    let er=document.querySelectorAll('input[type="checkbox"]');
+    for(var i=1;i<er.length;i++){
+            if(er[i].checked){
+                let fin=er[i].id;                                 //remove row by fin (id)accessed 
+                document.getElementById(fin).remove();
+            }
+        }
+    
+    let ud=document.querySelectorAll("td#sn");
+    var inp=document.querySelectorAll('input[type="checkbox"].cb');
+    for(var t=0;t<ud.length;t++){
+        ud[t].innerHTML=t+1;
+        let parent=ud[t].parentNode;
+        parent.setAttribute("id",t+1);
+        console.log(parent.setAttribute("id",t+1));                     //updating sno and sorting rows
+        inp[t].setAttribute("id",t+1);
+    }
+    Update();
+}
+function Update(){
+    t=0;
+    let tot=document.querySelectorAll("td#spent");                    //total update
+    for(var j=0;j<tot.length;j++){
+                t+=Number(tot[j].className);
+    }
+    document.getElementById("res").innerHTML="₹ "+t+" /-";
+}
+function checkIt() {
+    var mycheckbox = document.getElementById("mc");
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = mycheckbox.checked;
+    });
+    console.log(checkboxes.length);
+    for(var i=1;i<checkboxes.length;i++){
+        
+    }
+}function Show() {
+    // Remove existing canvas element
+    var oldCanvas = document.getElementById("myChart");
+    oldCanvas.parentNode.removeChild(oldCanvas);
+
+    // Create new canvas element
+    var newCanvas = document.createElement("canvas");
+    newCanvas.id = "myChart";
+    document.getElementById("chart").appendChild(newCanvas);
+
+    let s = new Set();
+    let obj = {"Food": 0, "Transport": 0, "Party": 0, "Groceries": 0, "Clothing": 0, "Bills": 0, "Health care": 0, "Entertainment": 0};
+    let y = document.querySelectorAll("#categ");
+    let x = document.querySelectorAll("#spent");
+
+    y.forEach(function(el) {
+        s.add(el.innerHTML);
+    });
+
+    for (var i = 0; i < x.length; i++) {
+        let v = y[i].textContent.trim(); // Category (trimmed to remove leading/trailing whitespace)
+        let amt = parseInt(x[i].textContent); // Spent amount; using textContent to get the text content
+    
+        // Check if the category already exists in obj
+        if (obj.hasOwnProperty(v)) {
+            obj[v] += amt; // If exists, add the spent amount to the existing total
+        } else {
+            obj[v] = amt; // If not exists, set the spent amount as the total
+        }
+    }
+
+    const keys = Object.keys(obj);
+    const values = keys.map(key => obj[key]);
+    var barColors = ["#4031a3","#4031a3","#4031a3","#4031a3","#4031a3","#4031a3","#4031a3","#4031a3"];
+
+    // Create new chart
+    var ctx = newCanvas.getContext('2d');
+    new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: keys,
+            datasets: [{
+                backgroundColor: barColors,
+                data: values
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false,
+                },
+                title: {
+                    display: true,
+                    text: "Analysis",
+                    color:  '#443a85',
+                    font:{
+                        size: 40,
+                        color: 'black'
+                    }
+                },
+            }
+            
+        }
+    });
+}
